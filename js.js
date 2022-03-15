32c0e672cf201fea25784f954240b120180027e1
AITechChannel <dta.bk13@gmail.com>
2022-03-15 17:23:04 +0700

    test

---
+++ js.js
@@ -0,0 +1,33 @@
+// var img = document.querySelector('.slider-img')
+// // function loop() {
+// //     img.setAttribute('src', 'https://www.w3schools.com/w3images/chicago.jpg')
+// //     setTimeout(() => {
+// //             img.setAttribute('src','https://www.w3schools.com/w3images/la.jpg')
+// //             setTimeout(() => {
+// //                 img.setAttribute('src','https://www.w3schools.com/w3images/ny.jpg')
+// //                 setTimeout(() => {
+// //                     loop();
+// //                 }, 1500);
+// //             }, 1500);
+// //     }, 1500);
+// // }
+// // loop()
+//     img.setAttribute('src', 'https://www.w3schools.com/w3images/chicago.jpg');
+//     var a = 0
+// setInterval(() => {
+//     a++;
+//     img.setAttribute('src', 'https://www.w3schools.com/w3images/chicago.jpg');
+//     if(a==1) {
+//         img.setAttribute('src','https://www.w3schools.com/w3images/la.jpg')
+//     }
+//     if(a==2) {
+//         img.setAttribute('src','https://www.w3schools.com/w3images/ny.jpg')
+//     }
+//     if(a == 3) {
+//         clearInterval();
+//         a = 0;
+//     }
+//     console.log(a)
+// }, 1500);
+
+
