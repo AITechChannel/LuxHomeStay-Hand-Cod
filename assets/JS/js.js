efcb8abf7c0c1ac21daa5de5ff258822766fd296
AITechChannel <dta.bk13@gmail.com>
2022-03-31 15:02:24 +0700

    90%

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -1,4 +1,5 @@
-var navIcon = document.querySelector('.nav-icon')
+var navIconOpen = document.querySelector('.nav-icon i:first-child')
+var navIconClose = document.querySelector('.nav-icon i:last-child')
 var slidebar = document.querySelector('.slidebar-left')
 var close = document.querySelector('.slidebar-close i')
 var overlay = document.querySelector('.overlay-slidebar')
@@ -12,14 +13,36 @@ var playVideo = document.querySelector('.play-icon')
 var video = document.querySelector('.video-popup video')
 
 // xử lý slidbar trượt bên trái qua
-navIcon.addEventListener('click', function() {
-    slidebar.style.right = '0'
-    overlay.style.display = 'block'
-})
-close.addEventListener('click', function() {
-    slidebar.style.right = '-350px'
-    overlay.style.display = 'none'
-})
+const pc = window.matchMedia("(min-width: 993px)");
+pc.addListener(handleDeviceChange);
+function handleDeviceChange(e) {
+  if (e.matches) {
+    navIconOpen.addEventListener('click', function() {
+        slidebar.style.right = '0'
+        overlay.style.display = 'block'
+    })
+    close.addEventListener('click', function() {
+        slidebar.style.right = '-350px'
+        overlay.style.display = 'none'
+    })
+} else {
+    const slidbarTop = document.querySelector('.slidebar-top')
+        navIconOpen.onclick = function () {
+            slidbarTop.style.top = '70px';
+            navIconOpen.style.display ='none'
+            navIconClose.style.display = 'block'
+        }
+        navIconClose.onclick = function () {
+            slidbarTop.style.top = '-320px'
+            navIconClose.style.display = 'none'
+            navIconOpen.style.display = 'block'
+            
+        }
+    }
+}
+handleDeviceChange(pc)
+
+
 // Show media video play
 overlay.onclick = function() {  
     slidebar.style.right = '-350px'
@@ -31,6 +54,8 @@ playVideo.onclick = function() {
 overlayVideo.onclick = function() {     
     overlayVideo.classList.remove('show')
 }
+
+
 //xử lý nút nhấn chuyển trang ok
 navBtns.forEach(function(navBtn, index) {
     navBtn.onclick = function() {
@@ -44,6 +69,8 @@ navBtns.forEach(function(navBtn, index) {
         aboutText.classList.add('active')
     }
 })
+
+
 // Xử lý hover vào thẻ tag work
 var imgWorks = document.querySelectorAll('.img-box')
 var overlayImg =document.querySelectorAll('.overlay-img')
@@ -57,6 +84,8 @@ imgWorks.forEach(function(imgWork) {
         imgWork.querySelector('.overlay-img').classList.remove('visible')    
     })
 })
+
+
 //Xử lý phân nhóm từng thẻ tab mỗi nội dung riêng biệt
 var tabWork = document.querySelectorAll('.tab')
 tabWork.forEach(function(tab){
@@ -76,8 +105,91 @@ tabWork.forEach(function(tab){
             } if (tab.getAttribute('group') =='marketing' && tag.getAttribute('group') == 'marketing') {        
                 tag.classList.add('show')
             } 
-
         })
-    }
-    
+    }   
 })
+
+
+//---------------Auto slider------------------
+var sliderItem = document.querySelectorAll('.testimonial')
+var slidebox = document.querySelector('.testimonials-box')
+var sliderItemWidth = sliderItem[0].offsetWidth;
+var sliderLength = sliderItem.length;
+setInterval (autoSlide, 2000)
+var i = 1;
+function autoSlide() {
+    if (i == 0) {
+        sliderItem[1].style = "left:595px;z-index:3";
+        
+        sliderItem[sliderLength - 1].style = "left:-595px;z-index:-2";
+    
+        sliderItem[0].style = "left:0;z-index:3";
+
+        sliderItem[sliderLength - 2].style = "left: 1190px;z-index:-2";
+
+        i++;
+
+      } else if (i == 1) {
+          
+        sliderItem[0].style = "left:-595px;z-index:3";
+        
+        sliderItem[1].style = "left:0;z-index:3";
+        
+        sliderItem[2].style = "left:595px;z-index:3";
+
+        sliderItem[3].style = "left: 1190px;z-index:-2";
+
+        i++;
+
+      } 
+      else if (i >= 2 && i < sliderLength - 1) {
+          
+        sliderItem[i - 2].style = "left: 1190px;z-index:-2";
+
+        sliderItem[i - 1].style = "left:-595px;z-index:3";
+        
+        sliderItem[i].style = "left: 0;z-index:3";
+        
+        sliderItem[i + 1].style = "left:595px;z-index:3";
+ 
+        i++;
+
+      }
+       else if (i === sliderLength -1) {
+           
+        sliderItem[0].style = "left: 595px;z-index:3";
+                
+        sliderItem[i - 1].style = "left:-595px;z-index:-2";
+        
+        sliderItem[i].style = "left:0;z-index:3";
+        
+        sliderItem[i - 2].style = "left: 1190px;z-index:-2";
+
+        i = 0;
+    
+      }
+}
+// -------------hieght box parent position: relative
+
+
+var testimonialBox = document.querySelector('.testimonials-box')
+function boxHeight() {
+    const slideItemHeight = sliderItem[0].offsetHeight;
+    testimonialBox.style.height = `${slideItemHeight}px`
+}
+boxHeight()
+
+
+// --------------google map API --------------------
+function initMap() {
+    const bachkhoa = { lat: 16.07238682707813, lng: 108.15357676086444 }; 
+    const map = new google.maps.Map(document.getElementById("map"), 
+    {
+      zoom: 18,
+      center: bachkhoa,
+    });
+    const marker = new google.maps.Marker({
+      position: bachkhoa,
+      map: map,
+    });
+}
