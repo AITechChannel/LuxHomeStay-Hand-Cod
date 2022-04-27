e70eba3af67efb676d407f70555185535360628b
AITechChannel <dta.bk13@gmail.com>
2022-04-27 18:00:05 +0700

    slide ok except drag

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -23,6 +23,7 @@ searchCustomer.onclick = function() {
 
 
 const bannerContainer = $('.banner-container')
+// console.log(bannerContainer.offsetWidth)
 const bannerImgs = $$('.banner-container img')
 
     bannerImgs[0].style.width = bannerContainer.offsetWidth + 'px'
@@ -30,23 +31,73 @@ const bannerImgs = $$('.banner-container img')
 
 const imgControlDot = $$('.img-control__dot')
 
-function slider() {
-    setTimeout(() => {
-        bannerImgs[0].style.left = `-${bannerImgs[0].offsetWidth}px`
-        bannerImgs[1].style.left = 0
-        imgControlDot[1].classList.remove('on')
-        imgControlDot[0].classList.add('on')
-        console.log(1)
+// function slider() {
+//     setTimeout(() => {
+//         bannerImgs[0].style.left = `-${bannerImgs[0].offsetWidth}px`
+//         bannerImgs[1].style.left = 0
+//         imgControlDot[1].classList.remove('on')
+//         imgControlDot[0].classList.add('on')
+//         console.log(1)
 
-        setTimeout(() => {
-        bannerImgs[1].style.left = `${bannerImgs[1].offsetWidth}px`
-        bannerImgs[0].style.left = 0
-        imgControlDot[0].classList.remove('on')
-        imgControlDot[1].classList.add('on')
-            slider()
-        },3000)
-    }, 3000);
+//         setTimeout(() => {
+//         bannerImgs[1].style.left = `${bannerImgs[1].offsetWidth}px`
+//         bannerImgs[0].style.left = 0
+//         imgControlDot[0].classList.remove('on')
+//         imgControlDot[1].classList.add('on')
+//             slider()
+//         },3000)
+//     }, 3000);
     
+// }
+// slider()
+
+
+// ------------------carousel-----------------
+const popularContainer = $('.popular-container')
+const popularList = $('.popular-list')
+popularContainer.style.height = popularList.offsetHeight + 'px'
+
+const imgContainers = $$('.img-container')
+const cmpStyles = window.getComputedStyle(imgContainers[0])
+//Lấy style của element
+const paddingImg = parseInt(cmpStyles.getPropertyValue('padding-left'))
+
+// set width cho thẻ con khi dùng thuộc tính position absolute
+imgContainers.forEach(element => {
+    element.style.width = (0.2 * (popularContainer.offsetWidth + 2*paddingImg)) + 'px'
+});
+
+popularList.addEventListener('mousedown', function (e) {
+    // popularList.style.left = `${e.offsetX}px`
+})
+
+ 
+// function click trượt slider
+const prevImg = $('.img--prev')
+const nextImg = $('.img--next')
+
+
+
+
+nextImg.onclick = function() {
+    let overRight = popularList.offsetWidth + (popularList.offsetLeft + paddingImg);
+    console.log(overRight)
+    if( (overRight-popularContainer.offsetWidth + 2*paddingImg) > imgContainers[0].offsetWidth) {
+        
+        let move = popularList.offsetLeft + paddingImg
+        move += - imgContainers[0].offsetWidth
+        popularList.style.left = `${move}px`
+
+    }
+
 }
-slider()
+
+prevImg.onclick = function() {
+    if((popularList.offsetLeft + paddingImg) <0 ) {
+        let move = popularList.offsetLeft + paddingImg
+        move += imgContainers[0].offsetWidth
+        popularList.style.left = `${move}px`
+        console.log(popularList.offsetLeft)
+    }
+}
 
